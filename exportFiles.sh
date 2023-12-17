4de0a537571d79f02ba5ce4e7ee2046a3b52ec39
ImanolCiganda <imanol.ciganda@estudiante.uam.es>
2023-12-17 14:18:15 +0100

    Added a system for creating subfolders in the backupFiles folder

--- exportFiles.sh
+++ exportFiles.sh
@@ -6,21 +6,24 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 # read list of file paths and names from files.txt
-while IFS=';' read -r file_path file_name; do
-    file_all=$(eval echo "$file_path/$file_name")
+while IFS=';' read -r file file_folder; do
+    file_all=$(eval echo "$file")
+    folder_all=$(eval echo "$file_folder")
+    file_path=$(dirname "$file")
+    file_name=$(basename "$file_all")
 
   # check if file exists in backupFiles directory
-  if [ -f "./backupFiles/$file_name" ]; then
+  if [ -f "./backupFiles/$folder_all/$file_name" ]; then
     # create directory path if it doesn't exist
     mkdir -p $(eval echo "$file_path")
     # copy file to specified path
-    if rsync --itemize-changes --update "./backupFiles/$file_name" "$file_all" | grep -q ">f"; then
+    if rsync --itemize-changes --update "./backupFiles/$folder_all/$file_name" "$file_all" | grep -q ">f"; then
       echo -e " [${GREEN}OK${NC}] Copied $file_name to $file_path"
     else 
       echo -e " [${GREEN}OK${NC}] $file_name is up to date"
     fi
   else
-    echo -e " [${RED}ERROR${NC}] $file_name does not exist in ./backupFiles/"
+    echo -e " [${RED}ERROR${NC}] $file_name does not exist in ./backupFiles/$folder_all"
   fi
 done < ./backupFiles/files.txt
 
