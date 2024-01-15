b96b64eab3ed73e29fe913627d648e7afd13dead
ImanolCiganda <imanol.ciganda@estudiante.uam.es>
2024-01-15 18:17:12 +0100

    Calling subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use args::BaupArgs;
+use args::Command;
 
 mod args;
 mod commands{pub mod import_export;}
@@ -9,8 +10,18 @@ fn main() {
     // Parse the arguments using the clap utility
     let arguments = BaupArgs::parse();
 
+    match arguments.command {
+        Command::Import(options) => println!("Import ; {:?}", options),
+        Command::Export => println!("Export"),
+        Command::Diff => println!("Diff"),
+        Command::Commit(options) => println!("Commit ; {:?}", options),
+        Command::Push(options) => println!("Push ; {:?}", options),
+        Command::Pull(options) => println!("Pull ; {:?}", options),
+        Command::Edit => println!("Edit"),
+    }
+
     let r = commands::import_export::import("/home/imanol/.baup/files.txt");
     println!("{:?}",r);
 
-    println!("{:?}",arguments.command);
+    // println!("{:?}",arguments.command);
 }
